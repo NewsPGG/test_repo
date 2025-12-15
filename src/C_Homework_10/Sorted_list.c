3e15d7cb917f0d07d0de956c88a315934b5bac4c
Кирилл Симонов <kirillsimonov3@mail.ru>
2025-12-15 20:37:33 +0300

    small fixes

--- src/C_Homework_10/Sorted_list.c
+++ src/C_Homework_10/Sorted_list.c
@@ -6,8 +6,7 @@ int main()
     List* list = newList();
     int command;
 
-    while (1)
-    {
+    while (1) {
         printf("0 - Выход \n");
         printf("1 - Добавить элемент \n");
         printf("2 - Удалить элемент \n");
@@ -15,21 +14,18 @@ int main()
         printf("Выберите команду: ");
         scanf("%d", &command);
 
-        switch (command)
-        {
+        switch (command) {
         case 0:
             deleteList(list);
             return 0;
-        case 1:
-        {
+        case 1: {
             int value;
             printf("Введите значение: ");
             scanf("%d", &value);
             insertList(list, value);
             break;
         }
-        case 2:
-        {
+        case 2: {
             int idx;
             printf("Введите индекс: ");
             scanf("%d", &idx);
