d6619001e5897cae9bc78a01bc61ad651b8fc135
Кирилл Симонов <kirillsimonov3@mail.ru>
2025-12-15 13:57:11 +0300

    Changes

--- src/C_Homework_3/Task_1.c
+++ src/C_Homework_3/Task_1.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-int main() {
-    const int size;
+int main()
+{
+    int size;
     printf("Введите размер массива: ");
     scanf("%d", &size);
     int array[size];
 
     for (int i = 0; i < size; i++) {
+        printf("Введите элемент %d: ", i + 1);
         scanf("%d", &array[i]);
     }
 
@@ -15,7 +17,7 @@ int main() {
         if (array[i] == 0) {
             count++;
         }
-     }
+    }
 
     printf("%d\n", count);
 
