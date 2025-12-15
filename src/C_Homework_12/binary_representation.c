54f3aee4ca03261fcf8cfb8ce189c022481f767a
Кирилл Симонов <kirillsimonov3@mail.ru>
2025-12-15 19:56:38 +0300

    Add file with solution, tests ti file and CMake

---
+++ src/C_Homework_12/binary_representation.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+void to_binary_twos_complement(int num, int binary[8])
+{
+    char char_num = num;
+
+    for (int i = 7; i >= 0; i--) {
+        binary[7 - i] = (char_num >> i) & 1;
+    }
+}
+
+int add_binary_twos_complement(const int a_bin[8], const int b_bin[8],
+                               int sum_bin[8])
+{
+    int carry = 0;
+
+    for (int i = 7; i >= 0; i--) {
+        int s = a_bin[i] + b_bin[i] + carry;
+        sum_bin[i] = s % 2;
+        carry = s / 2;
+    }
+
+    return carry;
+}
+
+int from_binary_twos_complement(const int binary[8])
+{
+    char result = 0;
+
+    for (int i = 0; i < 8; i++) {
+        result = (result << 1) | binary[i];
+    }
+
+    return (int)result;
+}
+
+void print_binary(const int binary[8])
+{
+    for (int i = 0; i < 8; i++) {
+        printf("%d", binary[i]);
+    }
+}
+
+int main()
+{
+    int number1, number2;
+    int bin_number1[8], bin_number2[8], bin_summ[8];
+    int summ;
+
+    printf("Введите первое число: ");
+    scanf("%d", &number1);
+    printf("Введите второе число: ");
+    scanf("%d", &number2);
+
+    to_binary_twos_complement(number1, bin_number1);
+    to_binary_twos_complement(number2, bin_number2);
+
+    printf("\nДвоичное представление: \n");
+    printf("%d = ", number1);
+    print_binary(bin_number1);
+
+    printf("\n%d = ", number2);
+    print_binary(bin_number2);
+
+    printf("\nРезультат сложения: ");
+    print_binary(bin_summ);
+
+    summ = from_binary_twos_complement(bin_summ);
+
+    printf("\nВ десятичном виде: %d\n", summ);
+
+    return 0;
+}
---
+++ src/C_Homework_12/tests_to_binary_representation.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+void to_binary_twos_complement(int num, int binary[8]);
+int add_binary_twos_complement(const int a_bin[8], const int b_bin[8],
+                               int sum_bin[8]);
+int from_binary_twos_complement(const int binary[8]);
+
+int main()
+{
+    // Тест 1
+    printf("Тест 1: 5 + 6 = 11\n");
+    int bin1[8], bin2[8], sum[8];
+
+    to_binary_twos_complement(5, bin1);
+    to_binary_twos_complement(6, bin2);
+    add_binary_twos_complement(bin1, bin2, sum);
+    int result = from_binary_twos_complement(sum);
+
+    printf("Результат: %d\n", result);
+    printf("Статус: %s\n\n", result == 11 ? "ПРОЙДЕН" : "НЕ ПРОЙДЕН");
+
+    // Тест 2
+    printf("Тест 2: -5 + 2 = -3\n");
+
+    to_binary_twos_complement(-5, bin1);
+    to_binary_twos_complement(2, bin2);
+    add_binary_twos_complement(bin1, bin2, sum);
+    result = from_binary_twos_complement(sum);
+
+    printf("Результат: %d\n", result);
+    printf("Статус: %s\n\n", result == -3 ? "ПРОЙДЕН" : "НЕ ПРОЙДЕН");
+
+    // Тест 3
+    printf("Тест 3: -5 + (-7) = -13\n");
+
+    to_binary_twos_complement(-5, bin1);
+    to_binary_twos_complement(-7, bin2);
+    add_binary_twos_complement(bin1, bin2, sum);
+    result = from_binary_twos_complement(sum);
+
+    printf("Результат: %d\n", result);
+    printf("Статус: %s\n\n", result == -13 ? "ПРОЙДЕН" : "НЕ ПРОЙДЕН");
+
+    // Тест 4
+    printf("Тест 4: 0 + 0 = 0\n");
+
+    to_binary_twos_complement(0, bin1);
+    to_binary_twos_complement(0, bin2);
+    add_binary_twos_complement(bin1, bin2, sum);
+    result = from_binary_twos_complement(sum);
+
+    printf("Результат: %d\n", result);
+    printf("Статус: %s\n\n", result == 0 ? "ПРОЙДЕН" : "НЕ ПРОЙДЕН");
+
+    // Тест 5
+    printf("Тест 5: Проверка преобразования 10 в двоичный вид\n");
+
+    int binary[8];
+    to_binary_twos_complement(10, binary);
+
+    printf("Двоичное представление 10: ");
+    for (int i = 0; i < 8; i++) {
+        printf("%d", binary[i]);
+    }
+
+    int expected[8] = {0, 0, 0, 0, 1, 0, 1, 0};
+    int correct = 1;
+    for (int i = 0; i < 8; i++) {
+        if (binary[i] != expected[i]) {
+            correct = 0;
+            break;
+        }
+    }
+
+    printf("\nСтатус: %s\n\n", correct ? "ПРОЙДЕН" : "НЕ ПРОЙДЕН");
+
+    return 0;
+}
